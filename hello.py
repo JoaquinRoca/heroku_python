67ced55a35e6dffc34e7a6ab508f1df99076e7ad
drpowpow <apowers411@gmail.com>
2015-06-15 00:28:27 -0400

    added database and library to connect with database

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import os
 from flask import Flask
+import psycopg2
+import urlparse
 
 app = Flask(__name__)
 
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+con = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 @app.route('/')
 def hello():
-    return 'Testing- Hello World!'
+    return render_template("presentation.html")
