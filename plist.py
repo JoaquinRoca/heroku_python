9a4f3f0aa0e0bc44d23a9daf1b16cfaf61fbcabc
drpowpow <apowers411@gmail.com>
2015-06-15 22:42:32 -0400

    fixed key

--- plist.py
+++ plist.py
@@ -2,4 +2,4 @@ __author__ = 'Alicia'
 consumer_key = 'ygHMdyqI5Rp5U8ocszeg'
 consumer_secret = 'Z1inuYmTC53Fnf6Pv5KDlhhYB8iDXICDefjPwzLBQ'
 access_token = '545244617-rnvZoA6sc2X3kHmEkoD6rRnoC6k5iGZOdALoCU60'
-access_token_secret = '1lz9
+access_token_secret = '1lz9O3fAQ7PE0wPRTc4TCMdodaAkdH5wuNDWCDRYFqBcK'
